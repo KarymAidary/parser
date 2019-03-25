ada91439bc2ff127d6d246b41c3119de02c432f6
Айдари Карим <karimaidariaskm@yandex.ru>
2019-03-26 01:48:51 +0300

    refactoring

--- parser.rb
+++ parser.rb
@@ -2,103 +2,130 @@ require 'curb'
 require 'nokogiri'
 require 'csv'
 
+$image_path = "//img[@id='bigpic']/@src"
+$item_attributes = "//ul[contains(@class, 'attribute_radio_list')]//li//span"
+$item_name  = "//h1[contains(@class, 'product_main_name')]"
 
-def get_html url   
-    begin 
-        http = Curl::Easy.perform(url) do |http|
-            http.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
-        end  
-        html = Nokogiri::HTML(http.body_str)
-    rescue Curl::Err::HostResolutionError 
-         puts "No internet connection or invalid link"
-         retry 
+
+class Parser 
+    @@easy_options = {:follow_location => true}
+    @@multi_options = {:pipeline => Curl::CURLPIPE_HTTP1}
+    attr_accessor :url
+
+    def initialize()
+        @url = nil
     end
     
+    def get_url 
+        @url
+    end
+    
+    def url=(url)
+        @url = url
+    end
+
+    def http_request
+        http = Curl::Easy.perform(@url) do |http|
+            http.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
+        end  
+    end
+
+    def multi_request(*urls)
+        htmls = Array.new
+        Curl::Multi.get(*urls, @@easy_options, @@multi_options) do |url|
+            htmls.push(Nokogiri::HTML(url.body_str))
+        end
+        return htmls
+    end
+
+    def get_html
+        Nokogiri::HTML(http_request.body_str)
+    end
+
+    def get_item (item)
+        get_html.xpath(item).text.strip
+    end
 end
 
-
-def get_category_urls(url)
-    puts "Collect category pages ...."
+def get_category_urls url
+    parser = Parser.new
+    parser.url = url 
     category_urls = Array.new 
-    while url != "" 
-        html = get_html(url)
-        url = html.xpath("//link[@rel='next']/@href").text.strip   
-        if url != ""
-            category_urls.push(url)
-        end 
+    while parser.url != "" 
+        category_urls.push(parser.url)
+        parser.url = parser.get_item("//link[@rel='next']/@href")
     end
+    puts "Сollecting category pages ...."
     return category_urls
 end
 
-def get_items_urls(url)
+
+def get_items_urls url
+    parser = Parser.new
     items_urls = Array.new
-    easy_options = {:follow_location => true}
-    multi_options = {:pipeline => Curl::CURLPIPE_HTTP1}
-    category_urls = get_category_urls(url) 
-    puts "Collect all the products ...."
-    Curl::Multi.get(category_urls, easy_options, multi_options) do |url|
-        html = Nokogiri::HTML(url.body_str)
+    category_urls = parser.multi_request(get_category_urls(url))
+    category_urls.each do |html|
         html.xpath("//div[contains(@class, 'pro_first_box')]/a/@href").each do |url|
             items_urls.push(url.text)
         end
     end
+    puts "Сollecting all the products ...."
     return items_urls
 end    
 
 
-def parse_item(url)
-    image_path = "//img[@id='bigpic']/@src"
-    weight_array = "//span[contains(@class, 'radio_label')]"
-    price_array  = "//span[contains(@class, 'price_comb')]"
-    item_attributes = "//ul[contains(@class, 'attribute_radio_list')]//li//span"
-    item_name  = "//h1[contains(@class, 'product_main_name')]"
+def parse_item url
     data = Array.new
-    easy_options = {:follow_location => true}
-    multi_options = {:pipeline => Curl::CURLPIPE_HTTP1}
-    items_urls = get_items_urls(url)
-    puts "We collect data from each product ...."
-    Curl::Multi.get(items_urls, easy_options, multi_options) do |url|
+    parser = Parser.new
+    items_urls = parser.multi_request(get_items_urls(url))
+    items_urls.each do |html|
         product = Hash.new
-        html = Nokogiri::HTML(url.body_str)
-        items = html.xpath(item_attributes).map { |item| item.content }
+        items = html.xpath($item_attributes).map { |item| item.content }
         options = items.each_slice(2).to_a
-        html.xpath(item_name).each { |name| product[:name] = name.text.strip } 
-        html.xpath(image_path).each { |img| product[:img] = img.text }
+        html.xpath($item_name).each { |name| product[:name] = name.text.strip } 
+        html.xpath($image_path).each { |img| product[:img] = img.text }
         product[:options] = options
         data.push(product)
     end 
+    puts "We collect data from each product ...."
     return data
+end  
+
+
+def write_to_csv(*data, file_name)
+    data.each do |product|
+        CSV.open("#{file_name}.csv", "wb", {:col_sep => "\t"}) do |csv|
+            csv << ["Name", "Price", "Image"]
+            product.each do |h| 
+                h[:options].each do |option|
+                    item = Array.new
+                    item.push("#{h[:name]} - #{option[0]}")
+                    item.push(option[1])
+                    item.push(h[:img])
+                    csv << item
+                end
+            end
+        end
+    end
 end
-
-
-
-
 
 
 while true
     puts "Enter category link: "
     url = gets.chomp
     puts "Enter file name: "
-    file = gets.chomp
-    if url.empty? || file.empty?
+    file_name = gets.chomp
+    if url.empty? || file_name.empty?
         puts "You must fill in all fields!"
         redo
     end
     t1 = Time.now
-    puts "connect to #{url} ...."
-    items = parse_item(url)
-    puts "Write to csv ..."
-    CSV.open("#{file}.csv", "wb", headers: items.first.keys) do |csv|
-        items.each do |h| 
-            h[:options].each do |option|
-                item = Array.new
-                item.push("#{h[:name]} - " + "#{option[0]}, " + "#{option[1]}, " + "#{h[:img]}")
-                csv << item
-            end
-        end
-    end
+    puts "Connecting to #{url} ...."
+    data = parse_item(url)
+    write_to_csv(data, file_name)
+    puts "Writing to csv ..."
     t2 = Time.now
     delta = t2 - t1
-    puts "Load time: #{delta}, Amount of products: #{items.length}"
+    puts "Load time: #{delta}, Amount of products: #{data.length}"
     break
 end
